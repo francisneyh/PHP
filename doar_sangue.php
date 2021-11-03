dc506fc1a5fa655f052b9a77f24f38bcc3d93901
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-03 14:10:53 -0300

    Add files via upload

---
+++ doar_sangue.php
@@ -0,0 +1,17 @@
+<?php
+//Doar sangue
+
+    $idade = 30;
+    $peso = 60;
+   
+   if(($idade >= 16 and $idade <= 69) and ($peso >= 50))
+   {
+       echo "Atende os requisitos para doar sangue";
+   }
+   
+   else
+   {
+       echo "não pode doar sangue";
+   }
+   
+?>
