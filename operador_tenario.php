665e40a09cb6b0bc62cf6bfba0503bd78d4f1a9d
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-03 13:34:02 -0300

    Add files via upload

---
+++ operador_tenario.php
@@ -0,0 +1,12 @@
+<?php
+    $possui_cartao = true;
+    $desconto = false;
+    $valor = 100;
+    
+    
+    $teste = $possui_cartao ? 'sim' : 'nao';
+    
+    echo "possui cartao? $teste \n";
+    echo "tem desconto? $desconto\n";
+    
+?>
