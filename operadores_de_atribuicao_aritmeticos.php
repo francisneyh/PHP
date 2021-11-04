b81ffc94e8f8bd0826ed5d7dcf6f29c3937e97c7
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-04 10:32:48 -0300

    Add files via upload

---
+++ operadores_de_atribuicao_aritmeticos.php
@@ -0,0 +1,16 @@
+<?php
+//operadores de atribuição aritméticos +, -, *, /, %
+$numero = 10;
+$numero2 = 8;
+$x = 5;
+$y = 2;
+
+$numero %= 8;
+echo "\nNúmero = $numero";
+
+echo "\n\n";
+
+$x *= $y;
+echo $x;
+
+?>
