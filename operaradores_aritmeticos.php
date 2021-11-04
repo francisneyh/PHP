ac76fab3c89ddfdee48cf875b299e071e3c9a465
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-04 09:56:52 -0300

    Add files via upload

---
+++ operaradores_aritmeticos.php
@@ -0,0 +1,24 @@
+<?php
+//operadores aritméticos
+$n1 = 10;
+$n2 =  3;
+
+$soma = $n1 + $n2;
+$subtracao = $n1 - $n2;
+$multiplicacao = $n1 * $n2;
+$divisao = $n1 / $n2;
+$resto = $n1 % $n2;
+
+echo "A soma dos números 10 e 3 é $soma\n";
+echo "A subtracao dos números 10 e 3 é $subtracao\n";
+echo "A multiplicacao dos números 10 e 3 é $multiplicacao\n";
+echo "A divisao dos números 10 e 3 é $divisao\n";
+echo "o resto dos números 10 e 3 é $resto\\n";
+
+echo "soma: " . ($n1 + $n2) . "\n";
+echo "subtracao: " . ($n1 -$n2) . "\n";
+echo "multiplicacao: " . ($n1 * $n2) . "\n";
+echo "divisao: " . ($n1 / $n2) . "\n";
+echo "resto: " . ($n1 % $n2) . "\n";
+
+?>
