5dd2ba6f277874e17adb20dabe77585efe49b5cb
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-05 11:21:33 -0300

    Add files via upload

---
+++ estruturas_de_repeticao.php
@@ -0,0 +1,30 @@
+<?php
+//Estruturas (laços) de repetição
+$i = 1;
+
+/*echo "\ninicio do loop";
+//WHILE
+while($i <= 10)
+{
+    echo"$i\n";
+    $i++;
+
+}
+echo "\nfim do loop ";
+*/
+
+//DO WHILE
+/*
+do 
+{
+    echo"\n$i";
+    $i++;
+}while($i <= 10);
+*/
+
+//FOR 
+for($i = 1; $i <= 5; $i++)
+{
+    echo "$i\n";
+}
+?>
