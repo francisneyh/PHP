e3fd125b8ff081abdcbec1fe346c435c318e11bb
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2021-11-10 11:32:50 -0300

    Add files via upload

---
+++ encapsulamento.php
@@ -0,0 +1,72 @@
+<?php
+//Encapsulamento 
+//Conta = deposita, saca, transfere, exibi saldo, exibi cpf, exibi nome, define nome, define cpf
+
+class Conta
+{
+    private String $cpf;
+    private String $nome;
+    private float $saldo;
+
+    public function saca(float $valorSacar):void //get
+    {
+        if ($valorSacar > $this->saldo)
+        {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorSacar;
+    }
+
+    public function deposita(float $valorDepositar): void //get
+    {
+        if ($valorDepositar < 0)
+        {
+           echo "O valor precisa ser positivo";
+           return; 
+        }
+
+        $this->saldo += $valorDepositar;
+    }
+
+    public function transfere(float $valorTransferir, Conta $contaDestino): void 
+    {
+        if ($valorTransferir > $this->saldo)
+        {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorTransferir);
+        $contaDestino->depositar($valorTransferir);
+    }
+
+    public function exibeSaldo(): float //get
+    {
+        return $this->saldo;
+    }
+
+    public function exibeNome(): String //get
+    {
+        return $this->nome;
+    }
+
+    public function exibeCpf(): String //get
+    {
+        return $this->cpf;
+    }
+
+    public function defineNome(String $nomeTitular): void //set
+    {
+        $this->nome = $nomeTitular;
+
+    }
+
+    public function defineCpf(String $cpfTitular): void //set
+    {
+        $this->cpf = $cpfTitular;
+    }
+}
+
+?>
